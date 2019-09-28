f5316e445c4f752902ce6951fd4c614fb1b461ff
Shubham <shubhamsinha2050@gmail.com>
2019-09-28 23:35:08 +0530

    ngStyle and ngClass are use methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend for Product Hunt';
   name = "Shubham Sinha";
   today = new Date();
   bind = "bundes";
   counter = 0;
+  mychoice = "green";
+
+  ngOnInit() { 
+    console.log("conmponent Initiated");
+  }
+
 
   click = function (event: any) {
     console.log("clicked");
